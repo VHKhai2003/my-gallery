2a0243ef43cb984cb9289007aba1e3a09d10fedc
21120477 <vuonghuynhkhai4869@gmail.com>
2024-10-15 20:59:35 +0700

    deploy product

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GridPhotos from "./components/GridPhotos";
 import PhotoDetail from "./components/PhotoDetail";
+import Home from "./components/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<GridPhotos />} />
+        <Route path="/" element={<Home />} />
         <Route path="/photos" element={<GridPhotos />} />
         <Route path="/photos/:id" element={<PhotoDetail />} />
       </Routes>
